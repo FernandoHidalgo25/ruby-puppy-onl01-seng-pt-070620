0fb60c8bdbe3b0f5549bb38feacd1e3c8cbc380a
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-24 16:23:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,6 +12,20 @@ class Dog
     @@all
   end
   
+    def self.clear_all
+    @@all = [] 
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+
+  def save
+    @@all << self
+  end
+  
   def self.clear_all
     @all 
   end 
