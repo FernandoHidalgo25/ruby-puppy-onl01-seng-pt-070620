43aa1ed36694762ced74d20db209203407df75a2
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-24 16:27:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
-class Dog 
+class Dog
   @@all = []
- 
+
   def initialize(name)
     @name = name
     self.save
   end
- 
+
   def self.all
     @@all
   end
-  
-    def self.clear_all
+
+  def self.clear_all
     @@all = [] 
   end
 
@@ -23,8 +23,9 @@ end
   def save
     @@all << self
   end
-  
-  def self.clear_all
-    @all 
-  end 
-end
+
+  def name
+    @name
+  end
+
+end 
